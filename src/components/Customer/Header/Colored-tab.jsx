6fd4e2c7e3217fd0cb0477f7738681fe21fc9016
giabao25 <kan250403@gmail.com>
2024-05-20 13:20:56 +0700

    upload

---
+++ src/components/Customer/Header/Colored-tab.jsx
@@ -0,0 +1,98 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useState } from "react";
+
+export default function ColorTabs() {
+  const [value, setValue] = useState("one");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
+  const drawerList = (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem button>
+          <a href="#Collection" onClick={() => handleChange(null, "one")}>
+            <ListItemText primary="Collection" />
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a href="#For Man" onClick={() => handleChange(null, "two")}>
+            <ListItemText primary="For Man" />
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a href="#For Women" onClick={() => handleChange(null, "three")}>
+            <ListItemText primary="For Women" />
+          </a>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ width: "auto" }}>
+      {isMobile ? (
+        <>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+            {drawerList}
+          </Drawer>
+        </>
+      ) : (
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="secondary tabs example"
+        >
+          <Tab
+            value="one"
+            label="Collection"
+            component="a"
+            href="#collection"
+          />
+          <Tab value="two" label="For Man" component="a" href="#man" />
+          <Tab value="three" label="For Women" component="a" href="#women" />
+        </Tabs>
+      )}
+    </Box>
+  );
+}
---
+++ src/components/Customer/Header/header.jsx
@@ -0,0 +1,113 @@
+import LoginIcon from "@mui/icons-material/Login";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useEffect, useRef, useState } from "react";
+import CartIcon from "./CartIcon.jsx";
+import ColorTabs from "./Colored-tab.jsx";
+
+function Header() {
+  const [isSticky, setSticky] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 768px)"); // Tailwind 'md' breakpoint is 768px
+
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 20) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+
+    if (mediaQuery.matches) {
+      window.addEventListener("scroll", handleScroll);
+    }
+
+    const handleResize = () => {
+      if (mediaQuery.matches) {
+        window.addEventListener("scroll", handleScroll);
+      } else {
+        window.removeEventListener("scroll", handleScroll);
+        setSticky(false);
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleResize);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      mediaQuery.removeEventListener("change", handleResize);
+    };
+  }, []);
+
+  const [isSearchOpen, setSearchOpen] = useState(false);
+  const searchInputRef = useRef(null);
+
+  const toggleSearch = (event) => {
+    event.preventDefault();
+    setSearchOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isSearchOpen && searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  }, [isSearchOpen]);
+  return (
+    <header
+      className={`header-container flex gap-5 w-full text-black max-md:flex-wrap max-md:max-w-full items-end ${
+        isSticky ? "sticky" : ""
+      }`}
+    >
+      <img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/761a3e35369fc6c259c389bd4b678d676da11023c4860c23c48806e300fd51ea?apiKey=2cf111b7142f4a06bfb2b5c186f14037&"
+        className="shrink-0 max-w-full aspect-[0.88] w-[76px]"
+        alt=""
+      />
+      <div className="title-header flex-auto text-4xl leading-10">
+        <span className="font-bold text-gradient bg-clip-text text-transparent">
+          Z
+        </span>
+        <span className="text-gradient bg-clip-text text-transparent">
+          odiacGems
+        </span>
+      </div>
+      <nav className="flex gap-5 max-md:gap-2 items-center justify-between text-base leading-7  max-md:max-w-full">
+        <ColorTabs />
+        <div className="shrink-0 w-px border border-solid bg-neutral-500 border-neutral-500 h-[22px]" />
+        <a
+          href="#"
+          className="p-0 m-0 border-none bg-transparent cursor-pointer transition duration-300 ease-in-out transform hover:scale-125"
+          onClick={toggleSearch}
+        >
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/af2729b48b540aebe63c9e1602f27f3aa437c301f4e68fb007d942413ce0a6bc?apiKey=2cf111b7142f4a06bfb2b5c186f14037&"
+            alt="Search icon"
+            className="shrink-0 self-start border border-white border-solid aspect-square fill-black stroke-[0.75px] stroke-white w-[19px]"
+          />
+        </a>
+        {isSearchOpen && (
+          <TextField
+            id="standard-basic"
+            label="Search"
+            variant="standard"
+            inputRef={searchInputRef}
+          />
+        )}
+
+        <CartIcon />
+        <Button
+          variant="contained"
+          endIcon={<LoginIcon />}
+          style={{ backgroundColor: "black", color: "white" }}
+        >
+          Login
+        </Button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
